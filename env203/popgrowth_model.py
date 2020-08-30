c50991760eeb93e718e2de0004e673a183cbcd02
Yozpoz64 <samuel.kolston@gmail.com>
2020-08-31 08:58:43 +1200

    commenting and deleted redundant code

--- env203/popgrowth_model.py
+++ env203/popgrowth_model.py
@@ -1,15 +1,13 @@
 # ENVSCI 203 Population Growth Model
 # created for assignment 3
-#   -excel is a better program for this assignment imo
-#   -just created for fun and to understand the model better
 #
 # Created by Samuel Kolston
 # Created on: 290820
-# Last edited: 300820
+# Last edited: 310820
 
 import matplotlib.pyplot as plt
 
-
+# constants
 RD = 0.5
 K = 2000
 T = 50
@@ -17,6 +15,7 @@ N_0 = 1
 Q = 250
 E = 0.5
 
+# dict for model values and associated plot preferences
 plots = \
     {
         "unharvested":
@@ -60,15 +59,18 @@ plots = \
             },
     }
 
+# add time and carry capacity to lists for plotting
 for time in range(T + 1):
     plots["k"]["values"].append(K)
     plots["t"]["values"].append(time)
 
 
+# function that performs the base logistic calculation
 def gen_log(nt, growth_rate, carry_cap):
     return nt + growth_rate * nt * (1 - nt / carry_cap)
 
 
+# runs all models using gen_log with modifiers and checks for abs values
 for model in range(T):
     plots["unharvested"]["values"].append(gen_log(plots["unharvested"]["values"][-1], RD, K))
 
@@ -87,58 +89,33 @@ for model in range(T):
     plots["harvested_fe"]["effort line"].append(E * plots["unharvested"]["values"][-1])
 
 
-'''
-def logistic_harvesting(growth_rate, carry_cap, years, quota, effort):
-    for i in range(years):
-        # not harvesting
-        plots["unharvested"]["values"].append(plots["unharvested"]["values"][-1] + growth_rate *
-                                              plots["unharvested"]["values"][-1] *
-                                              (1 - plots["unharvested"]["values"][-1] / carry_cap))
-
-        # harvesting under fixed quota
-        nt_1_q = plots["harvested_fq"]["values"][-1] + growth_rate * plots["harvested_fq"]["values"][-1] \
-                 * (1 - plots["harvested_fq"]["values"][-1] / carry_cap) - quota
-        if nt_1_q >= 0:
-            plots["harvested_fq"]["values"].append(nt_1_q)
-        else:
-            plots["harvested_fq"]["values"].append(0)
-
-        # harvesting under fixed effort
-        nt_1_e = plots["harvested_fe"]["values"][-1] + growth_rate * plots["harvested_fe"]["values"][-1] * \
-                 (1 - plots["harvested_fe"]["values"][-1] / carry_cap) - effort * \
-                 plots["harvested_fe"]["values"][-1]
-        if nt_1_e >= 0:
-            plots["harvested_fe"]["values"].append(nt_1_e)
-        else:
-            plots["harvested_fe"]["values"].append(0)
-        plots["harvested_fe"]["actual harvest"].append(effort * nt_1_e)  # THIS DOES NOT WORK - needs the first value
-        plots["harvested_fe"]["effort line"].append(effort * plots["unharvested"]["values"][-1])
-
-
-logistic_harvesting(RD, K, T, Q, E)
-'''
+# create figure
 figure, subp = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
 plt.suptitle("Population of ____ over {} years".format(T), fontsize=14)
 
+# left subplot
 subp[0].set_ylabel("Population")
 subp[0].set_title("Unharvested")
 subp[0].plot(plots["unharvested"]["values"], label=plots["unharvested"]["label"], color=plots["unharvested"]["colour"])
 subp[0].legend(ncol=2, bbox_to_anchor=(0.5, -0.15), loc="upper center")
 subp[0].set_xlabel(plots["t"]["label"])
 
+# right subplot
 subp[1].set_title("Harvested")
 subp[1].plot(plots["harvested_fq"]["values"], label=plots["harvested_fq"]["label"], color=plots["harvested_fq"]["colour"])
 subp[1].plot(plots["harvested_fe"]["values"], label=plots["harvested_fe"]["label"], color=plots["harvested_fe"]["colour"])
 subp[1].legend(ncol=2, bbox_to_anchor=(0.5, -0.15), loc="upper center")
 subp[1].set_xlabel(plots["t"]["label"])
 
+# adjust both plots with spacing
 figure.tight_layout(pad=3)
 plt.subplots_adjust(left=None, bottom=0.2, right=0.9, top=None, wspace=None, hspace=None)
 
+# print model output
 print(plots["harvested_fq"]["values"])
 print(plots["harvested_fe"]["values"])
 print(plots["harvested_fe"]["actual harvest"])
 print(plots["harvested_fe"]["effort line"])
 
-
+# show figure and plots
 plt.show()
